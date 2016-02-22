dea44f1555182fa942300aca8973eb6a4b99fa87
wilxsv <wixl.sv@gmail.com>
2016-02-22 02:51:48 -0600

    core

---
+++ html/README.md
@@ -0,0 +1,4 @@
+html
+====
+
+A Symfony project created on January 14, 2016, 10:53 am.
---
+++ html/src/Matilti/CoreBundle/MatiltiCoreBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Matilti\CoreBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MatiltiCoreBundle extends Bundle
+{
+}
