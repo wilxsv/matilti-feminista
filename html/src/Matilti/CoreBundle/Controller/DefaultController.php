89d0736494a9037c8bc8db6d280db7cd4e064a2d
wilxsv <wixl.sv@gmail.com>
2016-04-20 01:21:07 -0600

    version 0.5 del template

--- html/src/Matilti/CoreBundle/Controller/DefaultController.php
+++ html/src/Matilti/CoreBundle/Controller/DefaultController.php
@@ -10,4 +10,9 @@ class DefaultController extends Controller
     {
         return $this->render('MatiltiCoreBundle:Default:index.html.twig');
     }
+    
+    public function correoAction()
+    {
+        return $this->render('MatiltiCoreBundle:Default:correo.html.twig');
+    }
 }
